06834eb4ff02fa65cb34ec6578d512c6d153b3af
MarioAGR <marioagr95@outlook.com>
2017-05-31 21:17:04 -0600

    AppleMaps -> GoogleMaps, login w/Facebook working, existential doubts

---
+++ finalProject/test.m
@@ -0,0 +1,9 @@
+//
+//  test.m
+//  finalProject
+//
+//  Created by Mario Alberto Guillén Rivas on 22/05/17.
+//  Copyright © 2017 Mario Alberto Guillén Rivas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
